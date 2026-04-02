2772a502a9a42c5e485dc9d94ae99390065503e1
Kaijie ZHU <3040202533@qq.com>
2026-04-02 21:49:01 +0800

    Initial commit: Java Spring Boot skeleton for Unique Finds

---
+++ src/main/java/com/storefinds/uniquefindsbackend/UniqueFindsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.storefinds.uniquefindsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniqueFindsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniqueFindsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/storefinds/uniquefindsbackend/UniqueFindsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.storefinds.uniquefindsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniqueFindsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
